804820571c6edfcd21f8594022d2eaa7f0842806
Hezekiah M. Carty <hez@0ok.org>
2018-06-27 16:59:39 -0700

    Fix cohttp body type compatibility

--- aws-s3-async/compat.mli
+++ aws-s3-async/compat.mli
@@ -1,3 +1,5 @@
 (**/**)
-include Aws_s3.Types.Compat with type 'a Deferred.t = 'a Async.Deferred.t
+include Aws_s3.Types.Compat
+  with type 'a Deferred.t = 'a Async.Deferred.t
+  with type Cohttp_deferred.Body.t = Cohttp_async.Body.t
 (**/**)
--- aws-s3-lwt/compat.mli
+++ aws-s3-lwt/compat.mli
@@ -1,1 +1,3 @@
-include Aws_s3.Types.Compat with type 'a Deferred.t = 'a Lwt.t
+include Aws_s3.Types.Compat
+  with type 'a Deferred.t = 'a Lwt.t
+  with type Cohttp_deferred.Body.t = Cohttp_lwt.Body.t
