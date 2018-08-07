9ccdfef91267c244d4e2816b966ef1172db8aff0
Anders Fugmann <anders@fugmann.net>
2018-08-07 12:55:04 +0200

    Remove timeout from test

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ function test {
     TEST=$(( TEST + 1))
     echo -n "$TEST. $1: "
     shift
-    timeout 10s $@
+    $@
     if [ $? -eq 0 ]; then
         echo "ok"
     else
@@ -28,10 +28,6 @@ function test {
     fi
 }
 
-function fail {
-    echo Failed: $1
-    exit 1
-}
 function cleanup {
     rm -f ${TEMP}
     rm -f ${PART}
@@ -43,57 +39,58 @@ trap cleanup EXIT
 function suite () {
     BIN=$1
     HTTPS=$2
+    RETRIES=$3
     echo "TEST $(basename $BIN) https=$HTTPS"
 
-    test "upload" ${BIN} cp --https=${HTTPS} $FILE s3://${BUKCET}/test
-    test "head" ${BIN} head --https=${HTTPS} s3://${BUKCET}/test
-    test "download" ${BIN} cp --https=${HTTPS} s3://${BUKCET}/test ${TEMP}
+    test "upload" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} $FILE s3://${BUKCET}/test
+    test "head" ${BIN} head --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test
+    test "download" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test ${TEMP}
     test "data" diff -u $FILE ${TEMP}
 
-    test "eu upload expect" ${BIN} cp -e --https=${HTTPS} $FILE s3://${EU_BUKCET}/test
-    test "eu head" ${BIN} head --https=${HTTPS} s3://${EU_BUKCET}/test
-    test "eu download" ${BIN} cp --https=${HTTPS} s3://${EU_BUKCET}/test ${TEMP}
+    test "eu upload expect" ${BIN} cp -e --https=${HTTPS} --retries=${RETRIES} $FILE s3://${EU_BUKCET}/test
+    test "eu head" ${BIN} head --https=${HTTPS} --retries=${RETRIES} s3://${EU_BUKCET}/test
+    test "eu download" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${EU_BUKCET}/test ${TEMP}
     test "eu data" diff -u $FILE ${TEMP}
 
-    test "upload expect" ${BIN} cp -e --https=${HTTPS} $FILE s3://${BUKCET}/test
-    test "head" ${BIN} head --https=${HTTPS} s3://${BUKCET}/test
-    test "download" ${BIN} cp --https=${HTTPS} s3://${BUKCET}/test ${TEMP}
+    test "upload expect" ${BIN} cp -e --https=${HTTPS} --retries=${RETRIES} $FILE s3://${BUKCET}/test
+    test "head" ${BIN} head --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test
+    test "download" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test ${TEMP}
     test "data" diff -u $FILE ${TEMP}
 
-test "download stream" ${BIN} cp -c 8209 --https=${HTTPS} s3://${BUKCET}/test ${TEMP}
+test "download stream" ${BIN} cp -c 8209 --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test ${TEMP}
     test "data" diff -u $FILE ${TEMP}
 
-    test "upload chunked expect" ${BIN} cp -e -c 8209 --https=${HTTPS} $FILE s3://${BUKCET}/test
-    test "download stream" ${BIN} cp -c 8209 --https=${HTTPS} s3://${BUKCET}/test ${TEMP}
+    test "upload chunked expect" ${BIN} cp -e -c 8209 --https=${HTTPS} --retries=${RETRIES} $FILE s3://${BUKCET}/test
+    test "download stream" ${BIN} cp -c 8209 --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test ${TEMP}
     test "data" diff -u $FILE ${TEMP}
 
-    test "multi_upload" ${BIN} cp --https=${HTTPS} -m $FILE s3://${BUKCET}/test
-    test "download" ${BIN} cp --https=${HTTPS} s3://${BUKCET}/test ${TEMP}
+    test "multi_upload" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} -m $FILE s3://${BUKCET}/test
+    test "download" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test ${TEMP}
     test "data" diff -u $FILE ${TEMP}
 
-    test "multi_upload chunked" ${BIN} cp -c 8209 --https=${HTTPS} -m $FILE s3://${BUKCET}/test
-    test "download" ${BIN} cp --https=${HTTPS} s3://${BUKCET}/test ${TEMP}
+    test "multi_upload chunked" ${BIN} cp -c 8209 --https=${HTTPS} --retries=${RETRIES} -m $FILE s3://${BUKCET}/test
+    test "download" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test ${TEMP}
     test "data" diff -u $FILE ${TEMP}
 
-    test "multi_upload chunked expect" ${BIN} cp -e -c 8209 --https=${HTTPS} -m $FILE s3://${BUKCET}/test
-    test "download" ${BIN} cp --https=${HTTPS} s3://${BUKCET}/test ${TEMP}
+    test "multi_upload chunked expect" ${BIN} cp -e -c 8209 --https=${HTTPS} --retries=${RETRIES} -m $FILE s3://${BUKCET}/test
+    test "download" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test ${TEMP}
     test "data" diff -u $FILE ${TEMP}
 
-    test "partial get" ${BIN} cp --https=${HTTPS} s3://${BUKCET}/test --first=1024 --last=1049599 ${TEMP}
+    test "partial get" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test --first=1024 --last=1049599 ${TEMP}
     test "partial data" diff -u ${PART} ${TEMP}
 
-    test "partial download stream" ${BIN} cp --https=${HTTPS} --first=1024 --last=1049599 s3://${BUKCET}/test ${TEMP}
+    test "partial download stream" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} --first=1024 --last=1049599 s3://${BUKCET}/test ${TEMP}
     test "partial data" diff -u ${PART} ${TEMP}
 
-    test "rm" ${BIN} rm --https=${HTTPS} ${BUKCET} "test"
-    test "upload" ${BIN} cp --https=${HTTPS} $FILE s3://${BUKCET}/test1
-    test "s3 cp" ${BIN} cp --https=${HTTPS} s3://${BUKCET}/test1 s3://${BUKCET}/test2
-    test "ls" ${BIN} ls --max-keys=1 --https=${HTTPS} ${BUKCET}
-    test "multi rm" ${BIN} rm --https=${HTTPS} ${BUKCET} "test1" "test2"
+    test "rm" ${BIN} rm --https=${HTTPS} --retries=${RETRIES} ${BUKCET} "test"
+    test "upload" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} $FILE s3://${BUKCET}/test1
+    test "s3 cp" ${BIN} cp --https=${HTTPS} --retries=${RETRIES} s3://${BUKCET}/test1 s3://${BUKCET}/test2
+    test "ls" ${BIN} ls --max-keys=1 --https=${HTTPS} --retries=${RETRIES} ${BUKCET}
+    test "multi rm" ${BIN} rm --https=${HTTPS} --retries=${RETRIES} ${BUKCET} "test1" "test2"
 }
 for HTTPS in false true; do
     for BIN in ${BINS}; do
         dune build $BIN || exit
-        suite $BIN $HTTPS
+        suite $BIN $HTTPS 3
     done
 done
