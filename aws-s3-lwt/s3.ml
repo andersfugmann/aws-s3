dbd46b05c386505ed8a866b5d7d5b55b814df280
Anders Fugmann <anders@fugmann.net>
2018-09-26 15:28:15 +0200

    Add note about increasing lwt's default buffer size

--- aws-s3-lwt/s3.ml
+++ aws-s3-lwt/s3.ml
@@ -1,5 +1,13 @@
 (** Lwt aware S3 commands.
     For API documentation
     @see <../../../aws-s3/Aws_s3/S3/Make/index.html>({!module:Aws_s3.S3.Make})
+
+    Lwt uses a default buffer size of 4096 bytes, which means that on high speed connections
+    lwt will make needlessly many read / write system calls.
+
+    When transferring data over high speed connections, it is recommended to increase
+    the global channel buffer size e.g: Lwt_io.set_default_buffer_size (128*1024).
+
+    Each operation will allocate at least 2*default_buffer_size.
 *)
 include Aws_s3.S3.Make(Io)
